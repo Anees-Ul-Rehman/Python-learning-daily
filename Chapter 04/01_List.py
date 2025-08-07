0b0f57950553ed5e13c12fb6a58a37f262a04964
Anees_Ul_Rehman <aneesulrehman2025@gmail.com>
2025-08-07 18:29:05 +0500

    I update my today learning practice

---
+++ Chapter 04/01_List.py
@@ -0,0 +1,23 @@
+#  LISTS AND TUPLES 
+
+# Python lists are containers to store a set of values of any data type.
+# Any type of data we can enter in the lists like 'integers', 'strigns','floating','booleans', and so on
+
+Friends=["apple", "banana", "grapes", "lemon", 123, 34.3]
+print(Friends[2])
+Friends[0]="ball"      # Unlike strings lists are mutable
+print(Friends[0])
+print(Friends) 
+print(Friends[5])
+
+
+# LIST INDEXING 
+# A list can be indexed just like a string. 
+
+l1=[7,8,10.7,'Anees Ul Rehman']
+b=l1[0]
+c=l1[1]
+# d= l1[20]
+e=l1[0:2]
+print(l1)
+print(b,c,e)

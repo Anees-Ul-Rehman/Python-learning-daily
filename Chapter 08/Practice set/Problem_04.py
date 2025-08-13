e025fb564232a1a5dac571e08a2592cf0596423f
Anees_Ul_Rehman <aneesulrehman2025@gmail.com>
2025-08-13 19:59:19 +0500

    updated my practice set chapter 08

--- Chapter 08/Practice set/Problem_04.py
+++ Chapter 08/Practice set/Problem_04.py
@@ -1,8 +1,19 @@
 # Q:4. Write a recursive function to calculate the sum of first n natural numbers.
 
-n=input("Enter number: ")
+
+
+# n=int(input("Enter number: "))
+# def sum(n):
+#     if(n==1):
+#         return 1
+#     return sum(n-1)+n
+#     print(sum(n))
+
+# print(sum(n))
+
+n=int(input("Enter number: "))
 def sum(n):
     if(n==1):
         return 1
-    return sum(n-1)+n
-print(sum(67))
+    return(sum(n-1)+n)
+print(sum(n))
